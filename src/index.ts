39e376107718a0f56ed3c501e083dacf98d9be6b
Shahar Best <bestshahar9@gmail.com>
2022-06-12 15:32:29 +0300

    cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express, { Response } from 'express';
+import cors from 'cors';
 
 const port = process.env.PORT || 3001;
 
 const app = express();
+
+app.use(cors({ origin: '*' }))
 
 app.get('/', (_, res: Response) => {
 	res.status(200).send('hi');
